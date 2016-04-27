349b34335d81de3e8c77c3efaa94308221dd8873
Maxim Küpper <maxim@railslove.com>
2016-04-27 13:43:20 +0200

    improves tag handler / adds logging

--- nfc_bridge.rb
+++ nfc_bridge.rb
@@ -2,29 +2,18 @@ require './tag_handler'
 
 def call_url_with_uid(tag_uid)
   url = "http://cowokatra.apps.railslabs.com/cards/#{tag_uid.to_s}"
-  p "Opening URL: #{url}"
+  @logger.info "Opening URL: #{url}"
 
   # system "DISPLAY=:0 chromium-browser --kiosk --disable-session-crashed-bubble --noerrdialogs #{url}"
-  `open #{url}`
+  # `open #{url}`
 end
 
-stop_loop = false
+@logger = Logger.new(File.expand_path('logs/tag_helper.log', __dir__))
+
 tag_handler = TagHandler.new
-
-# Trap ^C 
-Signal.trap("INT") { 
-  stop_loop = true
-}
-
-# Trap `Kill `
-Signal.trap("TERM") {
-  stop_loop = true
-}
-
-while !stop_loop
-  tag_info = tag_handler.wait_for_tag 
+loop do
+  tag_info = tag_handler.wait_for_tag
   if tag_info
     call_url_with_uid(tag_info.to_s) if tag_info.to_s
   end
 end
-
--- tag_handler.rb
+++ tag_handler.rb
@@ -1,33 +1,34 @@
-require 'timeout'
 require 'nfc'
+require 'logger'
 
 class TagHandler
-  attr_reader :reader
+  attr_reader :reader, :logger
   attr_accessor :last_tag
 
-  def initialize(reader=nil)
+  def initialize
     # Create a new context
     ctx = NFC::Context.new
+    @logger = Logger.new(File.expand_path('logs/tag_helper.log', __dir__))
 
     # Open the first available USB device
     @reader = ctx.open reader
+  rescue RuntimeError
+    @logger.error 'Reader missing'
+    exit!
   end
 
   def wait_for_tag(wait_timeout = 1)
-    tag_info = nil
-
-    Timeout::timeout(wait_timeout) do 
-      tag_info = @reader.select
-    end
-
-    handle_new_tag tag_info if tag_info
-  rescue Timeout::Error
-    false
+    handle_new_tag reader.poll(1, 2)
   end
 
   def handle_new_tag(tag_info)
-    return false if tag_info.to_s == @last_tag.to_s
-
+    if ['0', @last_tag.to_s].include? tag_info.to_s
+      return false
+    elsif tag_info.to_s.to_i < 0
+      logger.error 'Reader gone'
+      exit!
+    end
+    logger.debug "It's a new Tag: #{tag_info.to_s}"
     @last_tag = tag_info
-  end 
-end 
+  end
+end
